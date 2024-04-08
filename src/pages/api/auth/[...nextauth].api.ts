cb49d77ff2acf387bdf7e611b9eb609767f093e7
Victor Matheus <victormatheus507@gmail.com>
2024-04-07 21:28:36 -0300

    conteudo da sessao do next auth

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -41,6 +41,14 @@ export function buildNextAuthOptions(
 
         return true;
       },
+      async session({session, user}) {
+        return {
+          ...session,
+          user,
+
+        }
+      }
+      
     },
   };
 }

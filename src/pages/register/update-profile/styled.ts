3fa131c5ecd83171651bc78b7e5521097eb22bef
Victor Matheus <victormatheus507@gmail.com>
2024-04-08 00:38:10 -0300

    pagina de atualizacao do perfil

---
+++ src/pages/register/update-profile/styled.ts
@@ -0,0 +1,21 @@
+import { Box, Text, styled } from "@ignite-ui/react";
+
+export const ProfileBox = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$4',
+
+  label: {
+    marginTop: '$6',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '$2  ',
+  
+  }
+  
+})
+
+export const FormAnnontatino = styled(Text, {
+  color: '$gray200'  
+})  
